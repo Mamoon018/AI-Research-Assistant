f1428f9388814796a1e4c3c96558ba125d006a42
Mamoon018 <mamonhaider11@gmail.com>
2025-06-01 13:38:21 +0200

    Project structure completed

---
+++ src/agents/research_assistant/nodes.py
@@ -0,0 +1,12 @@
+
+"""
+We are going to define all nodes for the agent in this nodes.py.
+"""
+
+import json 
+from typing import Any 
+
+from src.agents.research_assistant.state import Keywordstate   # Import State
+from langchain_core.messages import HumanMessage # To pass the prompt to LLM
+from langgraph.config import get_stream_writer # To show custom message in streaming 
+
--- src/agents/research_assistant/state.py
+++ src/agents/research_assistant/state.py
@@ -3,6 +3,35 @@
 
 from typing import Any, Literal
 from langgraph.graph import MessagesState
+from langchain.schema import Document  # To store document in state object
+from langchain_community.vectorstores import SupabaseVectorStore
 
+class Keywordstate(MessagesState):
 
+    # Input for node-1: User question related to the document that will be uploaded.
+    user_query: str 
+
+    # Input for node-1: Document that user will be uploading & want to inquire about.
+    user_doc: list[Document]
+
+    # Output of node-1: It contains the list of page-wise document objects containing page-content & metadata. For example: Documents: [Document(page_content="Text from page 1", metadata={"page": 1})]
+    Documents: list[Document]
+
+    # Output of node-1: It contains the list of chunk-wise document objects containing page-content & metadata.
+    Chunks: list[Document]
+
+    # Output of node-2: vectorstore contains the details of Supabase table & data in it.
+    Vectorstore: SupabaseVectorStore
+
+    # Output of node-3: It labels the query as if it requires DB,LLM or both.
+    Router_decision: Literal["DB", "LLM", "DB and LLM"]
+
+    # Output of node-4/5: Retrieved_data contains the list of chunks with page-content & metadata.
+    Retrieved_data: list[Document]
+
+    # Output of node-5: It stores the retrieved data and LLM analysis on the retrieved data.
+    LLM_analysis_on_retrieved_data: str 
+
+    # Output of node-6: It stores the LLM web-tool results plus LLM analysis on those results - in case user query is not related to the document uploaded.
+    LLM_generated_analysis: str 
 
