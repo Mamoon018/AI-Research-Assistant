85dc10e5ed734cf8756f3c81b1701f5d2b3cea58
Mamoon018 <mamonhaider11@gmail.com>
2025-06-04 13:16:36 +0200

    Web search SDK added

--- src/utils/model_initializer.py
+++ src/utils/model_initializer.py
@@ -1,11 +1,15 @@
 # In this file, we will initialize the LLMs with their parameters and Tools SDK that will be used throughout the app.
 
 from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+from typing import Literal
 from langchain_google_genai import ChatGoogleGenerativeAI
+from tavily import TavilyClient , AsyncTavilyClient
+from exa_py import Exa , AsyncExa
 from src.utils.settings import settings,get_api_key
 
 
-def get_openai_llm(model_num: int = 1, temperature: int = 0.5):
+def get_openai_llm(model_num: Literal[1,2] = 1 , temperature: int = 0.5):
     """
     It contains multiple openai models under usage and can switch between 
     by specifying just a model number. It will be useful when we would have to invoke two different openai models
@@ -20,25 +24,17 @@ def get_openai_llm(model_num: int = 1, temperature: int = 0.5):
     **Returns:**
     llm (ChatOpenAI): Openai llm initialized instance 
     """
-    model_num = {
+    model_list = {
         1: "gpt-4.1-mini",
         2: "o4-mini-2025-04-16" }
     
     # Let's get openai api key for llm
     openai_api_key = get_api_key(settings.OPENAI_API_KEY)
     
-    if model_num == 1:
-        openai_llm = ChatOpenAI(
-            model= "gpt-4.1-mini",
-            temperature= temperature,
-            max_completion_tokens= 400,
-            timeout= None,
-            max_retries= 2,
-            api_key= openai_api_key
-        )
-    else:
-        openai_llm = ChatOpenAI(
-            model= "o4-mini-2025-04-16",
+    openai_model = model_list[model_num]
+
+    openai_llm = ChatOpenAI(
+            model= openai_model,
             temperature= temperature,
             max_completion_tokens= 400,
             timeout= None,
@@ -47,8 +43,7 @@ def get_openai_llm(model_num: int = 1, temperature: int = 0.5):
         )
     return openai_llm
 
-
-def get_gemini_llm(model_num: int = 1, temperature: int = 0.5):
+def get_gemini_llm(model_num: Literal[1,2,3,4] = 1, temperature: int = 0.5):
     """
     It contains multiple gemini models that can be used in our project. To choose between different models 
     we just need to specify the model_num (model number).
@@ -67,12 +62,17 @@ def get_gemini_llm(model_num: int = 1, temperature: int = 0.5):
     # lets get gemini api key 
     gemini_api_key = get_api_key(settings.GEMINI_API_KEY)
 
-    gemini_models_list = {1:"gemini-2.0-flash", 2: "gemini-2.5-flash-preview-04-17",
-                     3: "gemini-2.5-pro-exp-03-25", 4: "gemini-1.5-pro"}
+    gemini_models_list = {
+        1:"gemini-2.0-flash", 
+        2: "gemini-2.5-flash-preview-04-17",
+        3: "gemini-2.5-pro-exp-03-25", 
+        4: "gemini-1.5-pro"
+                            
+                            }
+    
     gemini_model = gemini_models_list[model_num]
 
-    if model_num == 1:
-        gemini_llm = ChatGoogleGenerativeAI(
+    gemini_llm = ChatGoogleGenerativeAI(
             model = gemini_model,
             temperature=0,
             max_tokens=None,
@@ -81,6 +81,92 @@ def get_gemini_llm(model_num: int = 1, temperature: int = 0.5):
             google_api_key = gemini_api_key
         )
     return gemini_llm
+
+
+def get_groq_llm(model_num: Literal[1,2,3], temperature: int = 0.5) -> ChatGroq:
+    """
+    This function initializes the Groq llm that will be called in the nodes. 
+
+    **Args:**
+    model_num (int): It takes the integar as input that represent the model number associated with list of models 
+    in the dictionary. We can change it to switch among the following models.
+    (1) "llama3-70b-8192"
+    (2) "deepseek-r1-distill-llama-70b"
+    (3) "meta-llama/llama-4-scout-17b-16e-instruct"
+
+    temperature (int): It is the input to control the randomness of the model. Default value is set to 0.5
+
+    **Returns:**
+    ChatGroq: Initialized object of Groq llm.
+    """
+
+    # Lets get the Groq API
+    GROQ_API = get_api_key(settings.GROQ_API_KEY)
+
+    # Lets define the dictionary that contains the Groq models we can use.
+    Groq_model_list = {
+        1: "llama3-70b-8192",
+        2: "deepseek-r1-distill-llama-70b",
+        3: "meta-llama/llama-4-scout-17b-16e-instruct"
+                }
+    groq_model = Groq_model_list[model_num]
+
+    groq_llm = ChatGroq(
+        model= groq_model,
+        temperature= temperature,
+        max_tokens= None,
+        timeout= None,
+        max_retries= 2,
+        api_key= GROQ_API
+    )
+    return groq_llm
+
+
+
+def tavily_client_setup(asyncronous_tavily: bool = False) -> TavilyClient | AsyncTavilyClient:
+    """
+    Function returns the Tavily client. It can return both Syncronous and Asyncronous client
+
+    **Args:**
+    asyncronous_tavily (bool): It is set to False by default means we will get syncronous client & if passes True 
+                                it will return Asyncronous client.
+    
+    **Returns:**
+    TavilyClient or AsyncTavilyClient
+    """
+
+    TAVILY_API = get_api_key(settings.TAVILY_API_KEY)
+
+    if asyncronous_tavily:
+        return AsyncTavilyClient(api_key= TAVILY_API)
+    else:
+        return TavilyClient(api_key=TAVILY_API)
+    
+
+def Exa_client_setup(asyncronous_exa: bool = False) -> Exa | AsyncExa:
+    """
+    Function returns the Exa client. It can return both Syncronous and Asyncronous client
+
+    **Args:**
+    asyncronous_exa (bool): It is set to False by default means we will get syncronous client & if passes True 
+                                it will return Asyncronous client.
+    
+    **Returns:**
+    Exa or AsyncExa
+    """
+    
+    Exa_API = get_api_key(settings.EXA_API_KEY)
+
+    if asyncronous_exa:
+        return AsyncExa
+    else:
+        return Exa
+    
+
+
+
+    
+
     
 
 
